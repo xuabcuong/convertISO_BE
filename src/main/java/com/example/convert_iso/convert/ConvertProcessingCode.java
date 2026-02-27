e124a6df0dfa87612ae1f9dd480ea3b4d4ef8cf5
CUONG <xuabcuong@gmail.com>
2026-02-27 08:38:31 +0700

    fix file ConvertProcessingCode

--- src/main/java/com/example/convert_iso/convert/ConvertProcessingCode.java
+++ src/main/java/com/example/convert_iso/convert/ConvertProcessingCode.java
@@ -3,8 +3,8 @@ package com.example.convert_iso.convert;
 public class ConvertProcessingCode {
 
     public static String detectType(String code) {
-        if ( code.length() == 6) {
-            return "CARD";
+        if (code.length() == 6) {
+            return "";
         } else {
             return "ACCOUNT";
         }
